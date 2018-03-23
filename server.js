ab7f78682b3401db6f1aa777225caf3371b3ece8
Witold Olszyk <witoldolszyk@gmail.com>
2018-03-23 23:36:41 +0100

    add all task

---
+++ server.js
@@ -0,0 +1,27 @@
+
+var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var stringifyFile = '';
+
+app.use(bodyParser.json())
+
+app.get('/getNote', function(req, res) {
+    fs.readFile('./test.json', 'utf8', function(err, data) {
+      if (err) throw err;
+      stringifyFile = data
+      res.send(data);
+    });
+});
+
+app.post('/updateNote/:note', function(req, res) {
+   stringifyFile = req.params.note;
+  fs.writeFile('./test.json', stringifyFile, function(err) {
+  if (err) throw err;
+  console.log('file updated');
+  res.send(stringifyFile);
+  });
+});
+
+app.listen(3000);
